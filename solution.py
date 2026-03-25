cd31915fb458da80f0716f286cd9ee435fb08483
Praneshrvu <praneshpbsc24@rvu.edu.in>
2026-03-26 00:15:07 +0530

    Change input prompt from 'name' to 'names'
    Updated input prompt to request 'names' instead of 'name'.

--- solution.py
+++ solution.py
@@ -1,7 +1,7 @@
 # solution.py
 
 def solve():
-    name = input("Enter your name: ")
+    name = input("Enter your names: ")
     print("Hello,", name + "!")   # small change added "!"
 
 if __name__ == "__main__":
