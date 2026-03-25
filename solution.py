4e1be1e4ae38814b899fcc7d5bbaaa9d0b307cdf
Pranesh P <praneshpbsc24@rvu.edu.in>
2026-03-25 23:57:33 +0530

    Add improved logic on feature branch

--- solution.py
+++ solution.py
@@ -2,7 +2,7 @@
 
 def solve():
     name = input("Enter your name: ")
-    print("Hello,", name)
+    print("Hello,", name + "!")   # small change added "!"
 
 if __name__ == "__main__":
     solve()
